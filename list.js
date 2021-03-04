cd5e818a9a0e408c4d17c8deb7b5b3ec39b838e5
Amit Raina <amit.raina98@gmail.com>
2021-03-04 18:13:15 +0530

    First Commit

--- list.js
+++ list.js
@@ -1,21 +1,22 @@
 const page_Display = document.getElementById("display_page");
 
 
-
 const List = function(element, listItems){
 
     this.start = 0;
     this.itWas = 10;
 	this.pageSize = 10;
+    this.pageCount = Math.ceil(listItems.length/this.pageSize);
 	this.currentPage = 1;
 	this.render = function(){
+        console.log(this.start,this.itWas,this.pageSize,this.currentPage);
         page_Display.innerText = this.currentPage;
         element.innerHTML = `<tr>
             <th style="width: 100px;">Roll No</th>
             <th style="width: 200px;">Name</th>
             <th style="width: 200px;">Email Id</th>
         </tr>`;
-        for(let i=this.start;i<this.pageSize;i++){
+        for(let i=this.start;i<this.pageSize && i <= 40 ;i++){
             const createRow = document.createElement("tr");
 
             const roll_No = document.createElement("td");
@@ -69,7 +70,11 @@ const List = function(element, listItems){
 	
     this.navigatePrev = function(){
 
-        try{
+        // try{
+            if(this.currentPage == 1){
+                alert("First Page");
+                return;
+            }
 
             if(this.currentPage > 1)
             this.currentPage = this.currentPage - 1;
@@ -78,48 +83,62 @@ const List = function(element, listItems){
                 this.start = this.start - 10;
                 this.pageSize = this.pageSize - 10;
             }
-            
-            if(this.itWas == 20){
+            else if(this.itWas == 20){
+
                 this.start = this.start - 20;
                 this.pageSize = this.pageSize - 20;
             }
             
             this.render();
-        }
-        catch(e){
-            alert("Accessing values outside the table");
-            location.reload();
-        }
+        //}
+        // catch(e){
+        //     console.log(e.toString());
+        //     alert("Accessing values outside the table");
+        //     location.reload();
+        // }
 
     };
 	
     this.navigateNext = function(){
 
-        try{
+        // try{
+            if(this.currentPage == this.pageCount){
+                alert("Last Page");
+                return;
+            }
 
             if(this.currentPage < 4)
             this.currentPage = this.currentPage + 1;
+            else{
+                
+            }
+            
     
             if(this.itWas == 10){
                 this.start = this.start + 10;
                 this.pageSize = this.pageSize + 10;
             }
+            else if(this.itWas == 20){
             
-            if(this.itWas == 20){
                 this.start = this.start + 20;
                 this.pageSize = this.pageSize + 20;
+                
             }
-    
+            
             this.render();
-        }
-        catch(e){
-            alert("Accessing values outside the table");
-            location.reload();
-        }
+        //}
+        // catch(e){
+        //     console.log(e.toString());
+        //     alert("Accessing values outside the table");
+        //     location.reload();
+        // }
 
     };
 
     this.pageSizeChange = function(page_size){
+
+        page_size = parseInt(page_size);
+        this.pageCount = Math.ceil(listItems.length/page_size);
 
         if(page_size == 20 && this.start == 30 && this.pageSize == 40){
             this.start = this.start - 10;
@@ -140,12 +159,13 @@ const List = function(element, listItems){
 
             if(page_size == 10)
             this.itWas = 10;
-            else
+            else if(page_size == 20)
             this.itWas = 20;
             
             this.render();
             
          }   
+         
 
     };
 };
